74b4d8f00f5dd87fdf548c4414713c01141c0124
Hosein-NZF <HoseinnNazifi@gmail.com>
2024-12-02 17:15:13 +0330

    Remove package from some files

--- main/routes/clean.py
+++ main/routes/clean.py
@@ -1,16 +1,24 @@
 from flask import Blueprint, current_app
+from flask.json import jsonify
 
 
 clean_bp = Blueprint('clean', __name__, url_prefix='/engines/pspack')
 
 
-@clean_bp.route("/clean")
+@clean_bp.route('/clean')
 def clean():
-    """Clean all scans."""
-    return current_app.engine.clean()
+    """Clean all the scans."""
+    res = {"page": "clean"}
+
+    res.update({"status": "success"})
+    return jsonify(res), 200
 
 
-@clean_bp.route("/clean/<scan_id>")
+@clean_bp.route('/clean/<scan_id>')
 def clean_scan(scan_id):
-    """Clean scan identified by id."""
-    return current_app.engine.clean_scan(scan_id)
+    res = {"page": "clean"}
+
+    # ? clear tasks with scan id
+
+    res.update({"status": "removed"})
+    return jsonify(res), 200
